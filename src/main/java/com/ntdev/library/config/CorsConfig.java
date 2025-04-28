14fd0636acc6f5a38027b5806120f553ed57cdf8
bob <hoanganhan04@gmail.com>
2025-04-28 15:14:10 +0700

    Refactor CORS configuration for improved readability and maintainability

--- src/main/java/com/ntdev/library/config/CorsConfig.java
+++ src/main/java/com/ntdev/library/config/CorsConfig.java
@@ -5,21 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://library.thiendev.id.vn", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("Content-Type", "Referrer-Policy")
-                        .allowCredentials(true)
+@Configuration 
+public class CorsConfig { 
+    @Bean 
+    public WebMvcConfigurer corsConfigurer() { 
+        return new WebMvcConfigurer() { 
+            @Override 
+            public void addCorsMappings(CorsRegistry registry) { 
+                registry.addMapping("/**") 
+                        .allowedOrigins("https://library.thiendev.id.vn", "http://localhost:3000") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  
+                        .allowedHeaders("Content-Type", "Referrer-Policy") 
+                        .allowCredentials(true) 
                         .exposedHeaders("Set-Cookie")
-                        .maxAge(3600);
+                        .maxAge(3600); 
             }
         };
     }
